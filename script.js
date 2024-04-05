248bd3b7d78537fb31f7f81e0b6039ad58db3b96
William Knight <William.Jeffery.Knight@protonmail.com>
2024-04-05 10:36:05 -0400

    Keyboard inputs

--- script.js
+++ script.js
@@ -6,7 +6,21 @@ window.addEventListener("load", function () {
   canvas.height = 500;
 
   class InputHandler {
-
+    constructor(game){
+      this.game = game;
+      window.addEventListener('keydown', e =>{
+        if(((e.key === 'ArrowUp') || (e.key === 'ArrowDown')) && this.game.keys.indexOf(e.key) === -1){
+          this.game.keys.push(e.key)
+        }
+        console.log(this.game.keys);
+      window.addEventListener('keyup', e =>{
+        if(this.game.keys.indexOf(e.key) > -1){
+          this.game.keys.splice(this.game.keys.indexOf(e.key), 1)
+        }
+        console.log(this.game.keys);
+      })
+      })
+    }
   }
 
   class Projectile {
@@ -25,9 +39,13 @@ window.addEventListener("load", function () {
       this.x = 20;
       this.y = 100;
       this.speedY = 0;
+      this.maxSpeed = 5;
     }
     update(){
-      this.y += this.speedY
+      if(this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
+      else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
+      else this.speedY = 0;
+      this.y += this.speedY;
     }
     draw(context){
       context.fillRect(this.x, this.y, this.width, this.height);
@@ -54,7 +72,9 @@ window.addEventListener("load", function () {
     constructor(width, height){
       this.width = width;
       this.height = height;
-      this.player = new Player();
+      this.player = new Player(this);
+      this.input = new InputHandler(this);
+      this.keys = []
     }
     update(){
       this.player.update()
