cd9856d66fd9aebc9aabcedd9182d57538491c68
William Knight <William.Jeffery.Knight@protonmail.com>
2024-04-04 21:19:00 -0400

    Object Oriented programming with JavaScript

--- script.js
+++ script.js
@@ -2,6 +2,42 @@ window.addEventListener("load", function () {
   // canvas setup
   const canvas = this.document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
-  canvas.width = 1500;
+  canvas.width = 500;
   canvas.height = 500;
+
+  class InputHandler {
+
+  }
+
+  class Projectile {
+
+  }
+
+  class Particle {
+
+  }
+
+  class Player {
+    
+  }
+
+  class Ememy {
+
+  }
+
+  class Layer {
+
+  }
+
+  class Background {
+
+  }
+
+  class UI {
+
+  }
+
+  class Game {
+    
+  }
 });
