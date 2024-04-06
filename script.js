0a85d57125ce57fdbdd3e77f0ff4e12554658aae
William Knight <William.Jeffery.Knight@protonmail.com>
2024-04-06 00:17:03 -0400

    Drawing game score

--- script.js
+++ script.js
@@ -126,14 +126,22 @@ window.addEventListener("load", function () {
       this.game = game;
       this.frontSize = 25;
       this.fontFalimy = 'Helvetica';
-      this.color = 'yellow'
+      this.color = 'white'
     }
     draw(context){
-      // ammo
+      context.save();
       context.fillStyle = this.color;
+      context.shadowOffsetX = 2;
+      context.shadowOffsetY = 2;
+      context.shadowColor = 'black';
+      context.font = this.frontSize + 'px' + this.fontFalimy
+      //score
+      context.fillText("Score: " + this.game.score, 20, 40)
+      // ammo
       for(let i =0; i < this.game.ammo; i++){
         context.fillRect(20 + 5 * i, 50, 3, 20)
       }
+      context.restore();
     }
   }
 
@@ -153,6 +161,8 @@ window.addEventListener("load", function () {
       this.ammoTimer = 0;
       this.ammoInterval = 500;
       this.gameOver = false;
+      this.score = 0;
+      this.winningScore = 10;
     }
     update(deltaTime){
       this.player.update()
@@ -174,6 +184,7 @@ window.addEventListener("load", function () {
           if(enemy.lives <= 0){
             enemy.markedForDeletion = true;
             this.score += enemy.score;
+            if(this.score > this.winningScore) this.gameOver = true;
           }
         }
        })
@@ -213,7 +224,7 @@ window.addEventListener("load", function () {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     game.update(deltaTime);
     game.draw(ctx);
-    // requestAnimationFrame(animate);      
+    requestAnimationFrame(animate);      
   }
   animate(0);
 });
