39fc211fb3c6bcff49f51b7f8efa75acfe67324b
William Knight <William.Jeffery.Knight@protonmail.com>
2024-04-07 07:47:45 -0400

    Sprite animation with JavaScript

--- script.js
+++ script.js
@@ -54,9 +54,13 @@ window.addEventListener("load", function () {
       this.height = 190;
       this.x = 20;
       this.y = 100;
+      this.frameX = 0;//frame
+      this.frameY = 0;//row
+      this.maxFrame = 37;// number of frame counting from zero
       this.speedY = 0;
       this.maxSpeed = 5;
       this.projectiles = []
+      this.image = document.getElementById('player');
     }
     update(){
       if(this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
@@ -67,10 +71,17 @@ window.addEventListener("load", function () {
         projectile.update();
       });
       this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion)
+      // sprite animation
+      if (this.frameX < this.maxFrame){
+        this.frameX ++;
+      }else{
+        this.frameX = 0;
+      }
     }
     draw(context){
       context.fillStyle = 'black';
       context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height)
       this.projectiles.forEach(projectile => {
         projectile.draw(context)
       })
